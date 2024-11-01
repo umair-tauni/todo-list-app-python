4009bc1afbcb506c6f38112c13f42719d2ac5cfc
umair-tauni <engrumayr@gmail.com>
2024-11-01 12:25:52 +0500

    Add view method

--- todoList.py
+++ todoList.py
@@ -1,8 +1,9 @@
 
 class Todo:
     # empty list
-    tasks_list = []
+    todo_list = []
     total_todos = 0
+    
     # static method
     @staticmethod
     def welcome():
@@ -14,7 +15,7 @@ class Todo:
     def menu(self):
         try:
             print('1. Create todo')
-            print('2. View tdod')
+            print('2. View todo')
             print('3. Update todo')
             print('4. Delete todo')
             print('5. Exit program')
@@ -28,12 +29,19 @@ class Todo:
         total_tasks = int(input("Enter todo's: "))
         for i in range(1, total_tasks + 1):
             task = input(f'Enter task no {i}: ')
-            self.tasks_list.append(task)
+            self.todo_list.append(task)
             self.total_todos+=1
         print('--------------------------------')
-        # print(self.tasks_list)
-        print(('Tasks Saved: ' + ', '.join(str(i) for i in self.tasks_list)))
+        # print(self.todo_list)
+        print(('Tasks Saved: ' + ', '.join(str(i) for i in self.todo_list)))
         print('--------------------------------')
+
+    # view method
+    def view_todo(self):
+        print('-----------------------------------')
+        print('Total tasks:', self.total_todos)
+        print("Todo's:", self.todo_list)
+        print('-----------------------------------')
 
 # run static method
 Todo.welcome()
@@ -44,3 +52,5 @@ while True:
     result = todo.menu()
     if result == 1:
         todo.create_todo()
+    if result == 2:
+        todo.view_todo()
