a2c6d9948d68413d564ff7b296fdccc474730f5b
umair-tauni <engrumayr@gmail.com>
2024-11-01 12:16:26 +0500

    create empty list, add todo method, add while loop

--- todoList.py
+++ todoList.py
@@ -1,6 +1,8 @@
 
 class Todo:
-    
+    # empty list
+    tasks_list = []
+    total_todos = 0
     # static method
     @staticmethod
     def welcome():
@@ -20,10 +22,25 @@ class Todo:
             return choice
         except Exception as e:
             print(e)
+            
+    # create method
+    def create_todo(self):
+        total_tasks = int(input("Enter todo's: "))
+        for i in range(1, total_tasks + 1):
+            task = input(f'Enter task no {i}: ')
+            self.tasks_list.append(task)
+            self.total_todos+=1
+        print('--------------------------------')
+        # print(self.tasks_list)
+        print(('Tasks Saved: ' + ', '.join(str(i) for i in self.tasks_list)))
+        print('--------------------------------')
 
 # run static method
 Todo.welcome()
 # create object from class
 todo = Todo()
-
-    
+# while loop to run program continuously
+while True:
+    result = todo.menu()
+    if result == 1:
+        todo.create_todo()
