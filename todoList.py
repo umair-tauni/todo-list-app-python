a6782f443c89c0410df27e5c6f34b3a7828eb822
umair-tauni <engrumayr@gmail.com>
2024-11-01 11:38:26 +0500

    Add menu method

--- todoList.py
+++ todoList.py
@@ -7,4 +7,16 @@ class Todo:
         print('Welcome to Todo list app in Python')
         print('---------------------------------')
     
+    def menu(self):
+        try:
+            print('1. Create todo')
+            print('2. View tdod')
+            print('3. Update todo')
+            print('4. Delete todo')
+            print('5. Exit program')
+            choice = int(input('Choose: '))
+            return choice
+        except Exception as e:
+            print(e)
+        
     
